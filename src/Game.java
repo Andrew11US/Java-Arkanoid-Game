7fdbc4ad6df08b64d2528823b34efb085c25a514
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-04-17 23:21:31 +0200

    Added functional scoreboard

--- src/Game.java
+++ src/Game.java
@@ -5,9 +5,14 @@ import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class Game {
     public static Game game = new Game();
@@ -16,16 +21,17 @@ public class Game {
     public JFrame scoreFrame;
 //    private JLabel scoreLbl;
     public GamePanel gamePanel;
+    private ArrayList<String> scores = new ArrayList<>();
     private Thread thread;
     private boolean isRunning = false;
 
     private Game() {
         // Initializations
-        frame = new JFrame("Arkanoid v0.7");
+        frame = new JFrame("Arkanoid v0.9");
         scoreFrame = new JFrame("Scoreboard");
         gamePanel = new GamePanel();
         menuFrame = new JFrame();
-        JLabel nameLbl = new JLabel("Arkanoid v0.7", SwingConstants.CENTER);
+        JLabel nameLbl = new JLabel("Arkanoid v0.9", SwingConstants.CENTER);
         JButton startBtn = new JButton("Start");
         JButton scoreBtn = new JButton("Scoreboard");
         JButton exitBtn = new JButton("Exit");
@@ -60,17 +66,49 @@ public class Game {
 
         // TODO: Create functional scoreboard
         scoreBtn.addActionListener(listener -> {
-            scoreFrame.add(new ScoreLabel("HELLO"));
-            scoreFrame.add(new ScoreLabel("HELLO1"));
-            scoreFrame.add(new ScoreLabel("HELLO2"));
+            try (Stream<String> lines = Files.lines(Paths.get("scores.txt"), Charset.defaultCharset())) {
+                lines.forEachOrdered(line -> scores.add(line));
+            } catch (IOException e) {
+                // Error occurred reading the "scores.txt"
+                e.printStackTrace();
+            }
+            scores.sort(new Comparator<String>() { // Ascending order
+                @Override
+                public int compare(String o1, String o2) {
+                    int space1 = o1.lastIndexOf(" ");
+                    int space2 = o2.lastIndexOf(" ");
+                    return o1.substring(o1.length()-(o1.length()-space1)).compareTo(
+                            o2.substring(o2.length()-(o2.length()-space2)));
+                }
+            });
+            Collections.reverse(scores); // Make descending order
+
+            JLabel titleLbl = new JLabel("Scoreboard", SwingConstants.CENTER);
+            titleLbl.setFont(new Font("Calibri",Font.BOLD,32));
+            JTextArea textArea = new JTextArea(20,3);
+            textArea.setEnabled(false);
+//            textArea.setSize(Const.WINDOW_WIDTH,Const.WINDOW_HEIGHT - 30);
+            scores.forEach(item->{
+//                System.out.println(item);
+//                scoreFrame.add(new ScoreLabel(item));
+                textArea.append(item+System.lineSeparator());
+            });
+            JScrollPane scrollableTextArea = new JScrollPane(textArea);
+//            scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+//            scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+            scoreFrame.getContentPane().add(titleLbl, BorderLayout.NORTH);
+            scoreFrame.getContentPane().add(scrollableTextArea, BorderLayout.CENTER);
+            scoreFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
             JButton close = new JButton("Close");
+            close.setSize(Const.WINDOW_WIDTH,30);
             close.addActionListener(exit->{
                 scoreFrame.setVisible(false);
                 menuFrame.setVisible(true);
                 scoreFrame.removeAll();
             });
-            scoreFrame.add(close);
-            scoreFrame.setLayout(new GridLayout(4,1));
+            scoreFrame.add(close, BorderLayout.SOUTH);
+//            scoreFrame.setLayout(new GridLayout(11,1));
             scoreFrame.setVisible(true);
             menuFrame.setVisible(false);
 //            frame.add(scoreFrame);
@@ -125,7 +163,7 @@ public class Game {
         String name = JOptionPane.showInputDialog(str);
 
         if (name != null) {
-            String write = name + " " + score + "\n";
+            String write = name + " " + score + System.lineSeparator();
 
             try {
                 Files.write(Paths.get("scores.txt"), write.getBytes(), StandardOpenOption.APPEND);
